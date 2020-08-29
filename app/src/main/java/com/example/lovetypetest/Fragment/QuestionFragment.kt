160ce69be2773c28d180a01a70a39cbc36571e54
gmlwndla97 <juju5471@naver.com>
2020-08-29 19:56:09 +0900

    add fragment switching

--- app/src/main/java/com/example/lovetypetest/Fragment/QuestionFragment.kt
+++ app/src/main/java/com/example/lovetypetest/Fragment/QuestionFragment.kt
@@ -5,31 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.lovetypetest.R
+import kotlinx.android.synthetic.main.fragment_main.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class QuestionFragment : Fragment(), View.OnClickListener{
 
-/**
- * A simple [Fragment] subclass.
- * Use the [QuestionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class QuestionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    lateinit var navController: NavController //이따가 초기화하겠음
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -38,23 +21,20 @@ class QuestionFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_question, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment QuestionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            QuestionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+
+        button_next.setOnClickListener(this) //this의 의미 :class파일에서 구현한 것을 쓰겠다
     }
+
+    override fun onClick(p0: View?) {
+        when(p0?.id){
+            R.id.button_next->{
+                navController.navigate(R.id.action_questionFragment_to_selectionFragment)
+            }
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/lovetypetest/MainActivity.kt
+++ app/src/main/java/com/example/lovetypetest/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.lovetypetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var  navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = nav_host_fragment.findNavController()
+        //코틀린은 그냥 id써주면 된다.
     }
 }
